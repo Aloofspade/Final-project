5878d3d52137ec2bb610c44da1e157fd748c740d
Everett Sweet <sweetem2170@gmail.com>
2021-05-06 16:41:26 -0700

    Dylan is gay

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import HomePage from "./components/Home";
 function App() {
-  const [Home, setHome] = useState(true);
-  const [Daily, setDaily] = useState(false);
-  return (
+const [Home, setHome] = useState(true);
+const [Daily, setDaily] = useState(false);
+return (
     <div className="App">
-      <>
-        {Home && <HomePage onClick={() => {setHome(false), setDaily(true)}} />}
-      </>
+    <>
+        {Home && <HomePage onClick={() => {setHome(false); setDaily(true)}} />}
+    </>
     </div>
-  );
+);
 }
 
 export default App;
