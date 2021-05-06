2f8a2320248fa6e1fdedc86bb87d64c4f8433970
Dylan Thomas <gamingc340@gmail.com>
2021-05-05 18:04:41 -0700

    everette:

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+function App() {
+  const [Home, setHome] = useState(true);
+  const [Daily, setDaily] = useState(false);
+  return (
+    <div className="App">
+      <>
+        <div className="App">
+          <nav>
+            <ul>
+              <li onClick={() => {
+                setHome(true);
+                setDaily(false)
+              }}>stuff</li>
+              <li onClick={() => {
+                setHome(false)
+                setDaily(true)
+                }}>stuff</li>
+            </ul>
+          </nav>
+          {Home && <div className="home">
+            home
+          </div>}
+          {Daily && <div className="daily">
+            daily
+    </div>}
+        </div>
+      </>
+    </div>
+  );
+}
+
+export default App;
