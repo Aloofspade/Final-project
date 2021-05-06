d3eaadaaa96337ae441de35eeaccba4817347b65
Everett Sweet <sweetem2170@gmail.com>
2021-05-05 18:21:21 -0700

    Dylan is gay

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { createElement } from "react";
-
+import React, { useState } from "react";
 function App() {
-    return (
-        <>
-
-        </>
-    )
+const [Home, setHome] = useState(true);
+const [Daily, setDaily] = useState(false);
+return (
+    <div className="App">
+    <>
+        <div className="App">
+        <nav>
+            <ul>
+              <li
+                onClick={() => {
+                  setHome(true);
+                  setDaily(false);
+                }}
+              >
+                stuff
+              </li>
+              <li
+                onClick={() => {
+                  setHome(false);
+                  setDaily(true);
+                }}
+              >
+                stuff
+              </li>
+            </ul>
+          </nav>
+          {Home && <div className="home">home</div>}
+          {Daily && <div className="daily">daily</div>}
+        </div>
+      </>
+    </div>
+  );
 }
 
 export default App;
