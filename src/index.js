1f173b0398ceb5aa6f7dabb28c0391d308b266f6
Everett Sweet <sweetem2170@gmail.com>
2021-05-06 16:16:11 -0700

    Dylan is gay

--- src/index.js
+++ src/index.js
@@ -11,8 +11,4 @@ ReactDOM.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-<<<<<<< HEAD
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-=======
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
->>>>>>> 931567edbf850c66be25df83a2a444404bb19ef3
